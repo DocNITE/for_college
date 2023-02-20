d0aa1fbf981c042ed60de5241447a071cdd17623
Maxim Kozhevnikov <69177241+DocNITE@users.noreply.github.com>
2023-02-20 09:46:53 +0300

    Половина лабы 2

--- Anekdoti/MainWindow.xaml.cs
+++ Anekdoti/MainWindow.xaml.cs
@@ -22,9 +22,34 @@ namespace Anekdoti
     /// </summary>
     public partial class MainWindow : Window
     {
+        Canvas currWnd;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnSwitchWindow(object sender, RoutedEventArgs e)
+        {
+            Button _switchButton = (Button)e.Source;
+
+            for(int i = 0; i < this.cMainWindow.Children.Count; i++)
+            {
+                Canvas _elem = (Canvas)this.cMainWindow.Children[i];
+                Console.WriteLine("f. element: " + "c" + _switchButton.Name.Substring(1)
+                    + "\ns.element: " + _elem.Name);
+                if (_elem.Name == ("c" + _switchButton.Name.Substring(1)))
+                {
+                    Console.WriteLine("Finded?");
+                    if (currWnd != null)
+                    {
+                        currWnd.Visibility = Visibility.Hidden;
+                    }
+                    currWnd = _elem;
+                    currWnd.Visibility = Visibility.Visible;
+
+                    break;
+                }
+            }
         }
     }
 }
