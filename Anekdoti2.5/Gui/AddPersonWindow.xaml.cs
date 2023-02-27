0866f4182561fbc36222c431adb823338e06c58b
Maxim Kozhevnikov <69177241+DocNITE@users.noreply.github.com>
2023-02-27 11:20:05 +0300

    Половина 3й лабораторной
    Сделаны - удаление из списка (пока из сотрудников)
    
    В планах: редактирование и добавление новых сотрудников.

---
+++ Anekdoti2.5/Gui/AddPersonWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Anekdoti.Utils.Logs;
+
+using Database;
+
+namespace Anekdoti.Gui
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPersonWindow.xaml
+    /// </summary>
+    public partial class AddPersonWindow : Window
+    {
+        Person makingPerson;
+        MainWindow currentWnd;
+
+        public AddPersonWindow(MainWindow _wnd)
+        {
+            InitializeComponent();
+            currentWnd = _wnd;
+
+            LogInfo("Opened AddPersonWindow");
+        }
+    }
+}
