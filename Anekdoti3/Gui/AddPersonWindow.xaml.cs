4ce96ae10f9753c5e9d62a6a5606fea5f553b818
DocNight <69177241+DocNITE@users.noreply.github.com>
2023-03-06 11:09:56 +0300

    Yeao
    3 laba. Нету возможности редачить обьекты НО И ЛАДНО. Просто удалим и добавим новый. Мне пока запарно пилить UI

---
+++ Anekdoti3/Gui/AddPersonWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Anekdoti.Utils.Logs;
+using Anekdoti.ViewModel;
+
+using Database;
+
+/*
+ * this.ListPerson.Add(
+            new Person
+            {
+                Id = 1,
+                RoleId = roles.GetRoleWithId(1),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Birthday = new DateTime(1980, 02, 28)
+            });
+ */
+
+namespace Anekdoti.Gui
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPersonWindow.xaml
+    /// </summary>
+    public partial class AddPersonWindow : Window
+    {
+        MainWindow currentWnd;
+
+        public AddPersonWindow(MainWindow _wnd)
+        {
+            InitializeComponent();
+            currentWnd = _wnd;
+
+            LogInfo("Opened AddPersonWindow");
+        }
+
+        private void OnPersonAdd(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var firstName = this.PersonFirstName.Text;
+                var secondName = this.PersonSecondName.Text;
+                var datebirth = this.PersonDate.Text.Split('.');
+                var roleId = this.PersonRole.Text;
+
+                int nId = 0;
+                foreach (var item in currentWnd.vmPerson.ListPerson)
+                {
+                    if (item.Id >= nId)
+                    {
+                        nId = item.Id + 1;
+                    }
+                }
+
+                currentWnd.vmPerson.ListPerson.Add(new Person
+                {
+                    Id = nId,
+                    RoleId = currentWnd.vmRole.GetRoleWithId(int.Parse(roleId)),
+                    FirstName = firstName,
+                    LastName = secondName,
+                    Birthday = new DateTime(int.Parse(datebirth[2]), int.Parse(datebirth[1]), int.Parse(datebirth[0]))
+                });
+            }
+            catch 
+            {
+                // TODO: Надо сделать логгер об ошибке.
+                // но я хочу сделать для каждой переменной
+                // но я хз как организовать код, чтобы это не было жопой.
+            }
+            this.Close();
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Anekdoti3/Utils/Logs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anekdoti.Utils
+{
+    static class Logs
+    {
+        public static void Log(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+        }
+        public static void LogInfo(string message)
+        {
+            Log("[INFO]: ", ConsoleColor.Cyan);
+            Log(message + "\n");
+        }
+        public static void LogSystem(string message)
+        {
+            Log("[SYSTEM]: ", ConsoleColor.Yellow);
+            Log(message + "\n");
+        }
+        public static void NextLine()
+        {
+            Log("\n");
+        }
+    }
+}
